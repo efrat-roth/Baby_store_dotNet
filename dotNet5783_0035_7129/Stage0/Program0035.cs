a232af998a57fccb1ef16b8be41657140e9a23e0
efrat-roth <efratroth15@g.jct.ac.il>
2022-10-30 13:58:22 +0200

    intial code + welcome functions

---
+++ dotNet5783_0035_7129/Stage0/Program0035.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0035();
+            Welcome7129();
+        }
+        static partial void Welcome7129();
+        private static void Welcome0035()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write(name);
+            Console.Write(", welcome to my first consle application");
+        }
+    }
+}
+
+
+
+
