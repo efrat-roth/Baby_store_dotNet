8c031fe89c6d97a47800ff3554e34cfd9bbcaa43
YAEL-KAHANA <yael.kahana.2003@gmail.com>
2022-11-01 17:08:22 +0200

    deleting the red notices

--- dotNet5783_0035_7129/DalList/DataSource.cs
+++ dotNet5783_0035_7129/DalList/DataSource.cs
@@ -1,8 +1,8 @@
 ﻿namespace Dal;
 internal static class DataSource
 {
-    Random rand = new Random();
-    public static int readonly num=rand.Next(0,10000);
-	internal list<Product>;
+    
+    public static int num;
+	
  
 }
