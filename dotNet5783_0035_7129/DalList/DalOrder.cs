57e12b2b2f2d18cb0fcb81e716eb72b0b6886484
efrat-roth <efratroth15@g.jct.ac.il>
2022-11-07 17:38:38 +0200

    CRUD

--- dotNet5783_0035_7129/DalList/DalOrder.cs
+++ dotNet5783_0035_7129/DalList/DalOrder.cs
@@ -1,5 +1,19 @@
-﻿namespace Dal;
+﻿using System.Data;
+using DO;
+
+namespace Dal;
 
 public class DalOrder
 {
+    int Add(Order o) 
+    {
+        //check if this ID already exists
+        //if exists throw exception
+        //otherwise -go to list or array
+        //add obejct
+        //return 0; }
+    bool Update(Order o) { }
+    bool Delete(Order o) { }
+    Order GetByID(int ID);
+    IEnumerable<Order> GetOrders();
 }
--- dotNet5783_0035_7129/DalList/DalProduct.cs
+++ dotNet5783_0035_7129/DalList/DalProduct.cs
@@ -7,10 +7,15 @@ namespace Dal;
 
 public class DalProduct
 {
+    /// <summary>
+    /// Adding a product
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
     public int Add(Product p)
     {
-        products[nextEmptyProduct] = p;
-        nextCountProductID();
+        products[Config.nextEmptyProduct] = p;
+        Config.nextCountProductID();
         return 1;
     }
     
