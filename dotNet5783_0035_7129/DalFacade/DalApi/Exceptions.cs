9a54315f51e5257929ab285212c4c8288ee75e84
YAEL-KAHANA <yael.kahana.2003@gmail.com>
2022-11-13 13:30:26 +0200

    the exeptions class

--- dotNet5783_0035_7129/DalFacade/DalApi/Exceptions.cs
+++ dotNet5783_0035_7129/DalFacade/DalApi/Exceptions.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DO;
 
-namespace DalApi
+namespace DalApi;
+
+public class Exception
 {
-    internal class NotId
+    /// <summary>
+    /// Throwing the error problem. There is no id as requested.
+    /// </summary>
+    /// <returns></returns>
+    public string throwNotId()
     {
-        Console.WriteLine();
+        return "There is no id as you asked for";
     }
+    /// <summary>
+    /// Throwing the error problem. There is another is as requested.
+    /// </summary>
+    /// <returns></returns>
+    public string throwMoretId()
+    {
+        return "There is another id as you gave";
+    }
+    /// <summary>
+    /// Throwing the error problem.The list is empty.
+    /// </summary>
+    /// <returns></returns>
+    public string throwNotList()
+    {
+        return "There is another id as you gave";
+
+    }
+    /// <summary>
+    /// Throwing the error problem.The date is invalid.
+    /// </summary>
+    /// <returns></returns>
+    public string throwNoDate()
+    {
+        return "The date is invalid";
+
+    }
+
 }
