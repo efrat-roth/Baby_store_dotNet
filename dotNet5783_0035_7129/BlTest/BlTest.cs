4edf511bf4c2c5035191cbd4b2346d002b0abcdb
YAEL-KAHANA <yael.kahana.2003@gmail.com>
2022-11-21 22:31:27 +0200

    manage main

--- dotNet5783_0035_7129/BlTest/BlTest.cs
+++ dotNet5783_0035_7129/BlTest/BlTest.cs
@@ -1,13 +1,73 @@
 ﻿using BlApi;
 
-namespace BlTest
+namespace BlTest;
+
+internal class BlTest
 {
-    internal class BlTest
+    static void Main()
     {
-        static void Main()
+        IBl bl = new BL();
+        void mainActions()//The method ask the user to coose the main item by enim
         {
-            IBl bl = new BL();
-            
+            string choice = "start";
+            do
+            {
+                switch (choice)
+                {
+                    case "product":
+                        {
+                            manageProduct();
+                            break;
+                        }
+                    case "order":
+                        {
+                            manageOrder();
+                            break;
+                        }
+                    case "cart":
+                        {
+                            manageCart();
+                            break;
+                        }
+                    case "start":
+                        break;
+                    case "exit":
+                        {
+                            break;
+                        }
+
+                }
+                Console.WriteLine(
+                                       $@"Enter
+product to manage the products
+order to manage the orders
+cart to manage the items in the cart
+exit to exit the store");
+                choice = Console.ReadLine();
+
+            } while (choice != "exit");
         }
+
+        void manageProduct()
+        {
+            string choice = "start";
+
+        }
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }
