088b024b69ba143a346779a31e17ba8c2a7b3e03
efrat-roth <efratroth15@g.jct.ac.il>
2022-12-27 21:47:36 +0200

    Try Convert

--- dotNet5783_0035_7129/PL/OrderWindow.xaml.cs
+++ dotNet5783_0035_7129/PL/OrderWindow.xaml.cs
@@ -16,95 +16,118 @@ using BlApi;
 using BO;
 using Tools;
 
+
+
 namespace PL
 {
-
-    /// <summary>
-    /// Interaction logic for OrderWindow.xaml
-    /// </summary>
-    public partial class OrderWindow : Window
+    public class NotBooleanToVisibilityConverter : IValueConverter
     {
-        BlApi.IBl? _bl;
-        OrderDataBiding.Order order;
-        public OrderWindow(IBl bl1,BO.OrderForList o)
+        //convert from source property type to target property type
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            InitializeComponent();
-            _bl = bl1;
-            OrderDataBiding.Order order1 = new OrderDataBiding.Order()
+            bool boolValue = (bool)value;
+            if (boolValue)
             {
-                ID = o.ID,
-                Price = o.TotalPrice,
-                Name = o.CustomerName,
-                AmountOfItems = o.AmountOfItems,
-                Status = (BO.OrderStatus?)o.Status
-            };
-            order = order1;
-            OrderDetailsRows.DataContext = order;
-
-        }
-
-        private void UpdateProducts(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (GetProduct.Text.Length == 0 || amountContent.Text.Length == 0)
-                {//Input integrity check in case the uset didn't input the all details
-                    MessageBox.Show("Not all the details are typed");
-                    return;
-                }
-                int idProduct, amount;
-                int.TryParse(GetProduct.Text, out idProduct);
-                int.TryParse(amountContent.Text, out amount);
-                _bl?.Order.UpdateOrder(order.ID, idProduct, amount);
-                MessageBox.Show("The order has been successfuly updated");
-                this.Close();
+                return Visibility.Hidden; //Visibility.Collapsed;
             }
-            catch(CanNotDOActionException inner)
+            else
             {
-                MessageBox.Show("The order is already shiped");
-            }
-            catch (InvalidVariableException inner)
-            {
-                MessageBox.Show("The details are invalid, or the product is not in the order, please check again");
+                return Visibility.Visible;
             }
         }
-
-        /// <summary>
-        ///  Check the values of ID field, in order to get valid input
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void IdIsNumber(object sender, KeyEventArgs e)
+        //convert from target property type to source property type
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            TextBox? text = sender as TextBox;
-            if (text == null) return;
-            if (e == null) return;
-            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
-            if (char.IsControl(c)) return;
-            if (char.IsDigit(c))
-                if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt))) return;
-            e.Handled = true;
-            return;
-
-        }
-
-        /// <summary>
-        ///  Check the values of amount field, in order to get valid input
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void amountIsNumber(object sender, KeyEventArgs e)
-        {
-            TextBox? text = sender as TextBox;
-            if (text == null) return;
-            if (e == null) return;
-            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
-            if (char.IsControl(c)) return;
-            if (char.IsDigit(c))
-                if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt))) return;
-            e.Handled = true;
-            return;
-
+            throw new NotImplementedException();
         }
     }
+        /// <summary>
+        /// Interaction logic for OrderWindow.xaml
+        /// </summary>
+        public partial class OrderWindow : Window
+        {
+            BlApi.IBl? _bl;
+            OrderDataBiding.Order order;
+            public OrderWindow(IBl bl1, BO.OrderForList o)
+            {
+                InitializeComponent();
+                _bl = bl1;
+                OrderDataBiding.Order order1 = new OrderDataBiding.Order()
+                {
+                    ID = o.ID,
+                    Price = o.TotalPrice,
+                    Name = o.CustomerName,
+                    AmountOfItems = o.AmountOfItems,
+                    Status = (BO.OrderStatus?)o.Status
+                };
+                order = order1;
+                OrderDetailsRows.DataContext = order;
+
+            }
+
+            private void UpdateProducts(object sender, RoutedEventArgs e)
+            {
+                try
+                {
+                    if (GetProduct.Text.Length == 0 || amountContent.Text.Length == 0)
+                    {//Input integrity check in case the uset didn't input the all details
+                        MessageBox.Show("Not all the details are typed");
+                        return;
+                    }
+                    int idProduct, amount;
+                    int.TryParse(GetProduct.Text, out idProduct);
+                    int.TryParse(amountContent.Text, out amount);
+                    _bl?.Order.UpdateOrder(order.ID, idProduct, amount);
+                    MessageBox.Show("The order has been successfuly updated");
+                    this.Close();
+                }
+                catch (CanNotDOActionException inner)
+                {
+                    MessageBox.Show("The order is already shiped");
+                }
+                catch (InvalidVariableException inner)
+                {
+                    MessageBox.Show("The details are invalid, or the product is not in the order, please check again");
+                }
+            }
+
+            /// <summary>
+            ///  Check the values of ID field, in order to get valid input
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void IdIsNumber(object sender, KeyEventArgs e)
+            {
+                TextBox? text = sender as TextBox;
+                if (text == null) return;
+                if (e == null) return;
+                char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+                if (char.IsControl(c)) return;
+                if (char.IsDigit(c))
+                    if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt))) return;
+                e.Handled = true;
+                return;
+
+            }
+
+            /// <summary>
+            ///  Check the values of amount field, in order to get valid input
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void amountIsNumber(object sender, KeyEventArgs e)
+            {
+                TextBox? text = sender as TextBox;
+                if (text == null) return;
+                if (e == null) return;
+                char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+                if (char.IsControl(c)) return;
+                if (char.IsDigit(c))
+                    if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt))) return;
+                e.Handled = true;
+                return;
+
+            }
+        }
+    
 }
