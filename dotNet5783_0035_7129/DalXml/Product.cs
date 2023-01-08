8fb6a3520779ebf29f1aaf20868f897cd28f0600
YAEL-KAHANA <yael.kahana.2003@gmail.com>
2023-01-08 17:42:38 +0200

    turning product to xml

--- dotNet5783_0035_7129/DalXml/Product.cs
+++ dotNet5783_0035_7129/DalXml/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using DalApi;
 using DO;
 
@@ -10,5 +11,69 @@ namespace Dal;
 
 internal class Product : IProduct
 {
+    XElement ProductRoot;
+    string FPath = "C:\\Users\\משתמש\\source\\repos\\efrat-roth\\dotNet5783_0035_7129\\dotNet5783_0035_7129\\xml\\Product.xml";
+    public Product()
+    {
+        if (!File.Exists(FPath))
+            CreateFiles();
+        else
+            LoadData();
+    }
+    private void CreateFiles()
+    {
+        ProductRoot = new XElement("products");
+        ProductRoot.Save(FPath);
+    }
 
+    private void LoadData()
+    {
+        try
+        {
+            ProductRoot = XElement.Load(FPath);
+        }
+        catch
+        {
+            throw new Exception("File upload problem");
+        }
+    }
+    public void SaveProductListLinq(List<DO.Product> productsList)
+    {
+        var v = from p in productsList
+                select new XElement("product",
+                    new XElement("id", p.ID),
+                   new XElement("name",p.Name),
+                   new XElement("InStock", p.InStock),
+                   new XElement("Price", p.Price),
+                   new XElement("Category", p.Category)                  
+                    );
+        ProductRoot = new XElement("products", v);
+        ProductRoot.Save(FPath);
+    }
+    public List<DO.Product?> GetProductsList()
+    {
+        LoadData();
+        List<DO.Product> products;
+       
+        try
+        {
+            DO.Category c=new DO.Category();
+            products = (from p in ProductRoot.Elements()
+                        let b = Category.TryParse(p.Element("Category")!.Value, out c)
+                        let product1 = new DO.Product()
+                        {
+                            ID = Convert.ToInt32(p.Element("id")!.Value),
+                            Name = p.Element("name")!.Value,
+                            Price = Convert.ToDouble(p.Element("Price")!.Value),
+                            Category = c,
+                            InStock = Convert.ToInt32(p.Element("InStock")!.Value)
+                        } 
+                        select product1).ToList();
+        }
+        catch
+        {
+            products = null;
+        }
+        return products??throw new ObgectNullableException();
+    }
 }
